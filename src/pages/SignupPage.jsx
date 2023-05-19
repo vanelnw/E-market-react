71c10248784a470c2cc69ceed28bd4ad1e8ef46e
Vanel <vanelnwaba@gmail.com>
2023-05-19 19:23:25 +0100

    add signup page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div>
+      <h1>SignUp</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <p>Do not have an account?</p>
+        <button type='submit'/>
+      </form>
+    </div>
   )
 }
 
